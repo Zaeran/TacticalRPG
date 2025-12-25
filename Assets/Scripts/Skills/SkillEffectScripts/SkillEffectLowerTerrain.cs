7299581b09f67631661994efb63beef2c1321d59
Nathan Beattie <nathan@arkenforge.com>
2025-12-26 00:32:38 +1100

    Lower Terrain Skill
    - Added Lower Terrain skill
    - Added SkillEffectLowerTerrain

---
+++ Assets/Scripts/Skills/SkillEffectScripts/SkillEffectLowerTerrain.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillEffectLowerTerrain : ISkillEffect
+{
+
+    public void ProcessEffect(CharacterObject c, List<ClickableTarget> hitCharacters, Vector4 point)
+    {
+        DrawSquaresScript.DestroyValidSquares();
+        foreach (ClickableTarget hitObject in hitCharacters)
+        {
+            //if(hitObject is TerrainObject)
+            {
+                hitObject.transform.position += new Vector3(0, -1, 0);
+            }
+        }
+    }
+}
--- Assets/Scripts/Skills/SkillTrees/AllSkills.cs
+++ Assets/Scripts/Skills/SkillTrees/AllSkills.cs
@@ -12,6 +12,19 @@ public class AllSkills
 
         SliceSkill();
         ShootSkill();
+        RaiseTerrainSkill();
+    }
+
+    public static Skill GetSkillForName(string skillName)
+    {
+        for (int i = 0; i < _allSkills.Count; i++)
+        {
+            if (_allSkills[i].Name == skillName)
+            {
+                return _allSkills[i];
+            }
+        }
+        return null;
     }
 
     static void SliceSkill()
@@ -67,7 +80,28 @@ public class AllSkills
         skill.AddPrerequisite(prereq2);
         skill.SetTags(Skill.SkillTag.Attack);
         skill.SetTargeting(new SkillTargetMagic(3));
-        skill.AddSkillEffect(new SkillEffectWeaponAttack());
+        skill.AddSkillEffect(new SkillEffectRaiseTerrain());
+        SkillCostAP cost = new SkillCostAP();
+        cost.SetCostValue(2);
+        skill.AddSkillCost(cost);
+        SkillTargetRadiusAOE skillTarget = new SkillTargetRadiusAOE(0);
+        skillTarget.SetAOE(0);
+        skill.SetSkillTargetRadius(skillTarget);
+        _allSkills.Add(skill);
+    }
+
+    static void LowerTerrainSkill()
+    {
+        Skill skill = new Skill("Lower Terrain");
+        SkillPrereqAPCost prereq = new SkillPrereqAPCost();
+        prereq.SetPrereqProperty(3);
+        skill.AddPrerequisite(prereq);
+        SkillPrereqActiveSkillTree prereq2 = new SkillPrereqActiveSkillTree();
+        prereq2.SetPrereqProperty(SkillTree.Geomancy);
+        skill.AddPrerequisite(prereq2);
+        skill.SetTags(Skill.SkillTag.Attack);
+        skill.SetTargeting(new SkillTargetMagic(3));
+        skill.AddSkillEffect(new SkillEffectLowerTerrain());
         SkillCostAP cost = new SkillCostAP();
         cost.SetCostValue(2);
         skill.AddSkillCost(cost);

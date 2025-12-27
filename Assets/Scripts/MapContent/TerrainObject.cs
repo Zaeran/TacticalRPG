d48a4027819eac762f22b5b9e253cd6bb2ad92be
Nathan Beattie <nathan@arkenforge.com>
2025-12-28 00:01:15 +1100

    Terrain types
    - Added Rock and Water terrain types

--- Assets/Scripts/MapContent/TerrainObject.cs
+++ Assets/Scripts/MapContent/TerrainObject.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class TerrainObject : ClickableTarget
 {
+    TerrainType _myTerrainType;
 
+    public void Start()
+    {
+        if ((int)Random.Range(0, 5) == 2)
+        {
+            _myTerrainType = TerrainType.Water;
+        }
+        else
+        {
+            _myTerrainType = TerrainType.Rock;
+        }
+        
+        GetComponent<MeshRenderer>().material.color = _myTerrainType == TerrainType.Rock ? Color.gray : Color.blue;
+    }
 }
--- Assets/Scripts/Utilities/Enums.cs
+++ Assets/Scripts/Utilities/Enums.cs
@@ -2,3 +2,4 @@ public enum EquipmentType { Weapon, Armour, Accessory, Item};
 public enum WeaponTargetingType { Line, Range};
 public enum SkillTag { Movement, Attack, Physical, Magical, Buff, Debuff, Fire, Water, Earth, Air };
 public enum SkillTree { Blade, Block, Geomancy, Hydromancy, Aeromancy, Pyromancy, Blunt, Armour, Mobility, Potions, Skullduggery, Spear, Bow, Unarmed};
+public enum TerrainType { Rock, Water};
